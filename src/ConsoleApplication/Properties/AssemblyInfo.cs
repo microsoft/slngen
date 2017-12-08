48a2916c717a9ebc9ae452a92e7775db2dac4061
Jeff Kluge <jeffkl@microsoft.com>
2017-12-08 13:07:54 -0800

    Move stuff around, delete ConsoleApplication

--- src/ConsoleApplication/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Resources;
-
-[assembly: AssemblyTitle("SlnGen: the VS sln generator.")]
-[assembly: AssemblyDescription("Automatically generates Visual Studio solution files for a complete project closure or traversal.")]
-[assembly: NeutralResourcesLanguage("en-US")]
