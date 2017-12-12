b1b3dc8d155f271f30a1c0bd73bd673cb2de0609
Amr Hanafi <amramahdi@gmail.com>
2017-12-11 17:27:52 -0800

    Create a NuGet Package(#5)

--- src/SlnGen.Build.Tasks/Properties/AssemblyInfo.cs
+++ src/SlnGen.Build.Tasks/Properties/AssemblyInfo.cs
@@ -1,17 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("SlnGen.Build.Tasks")]
-[assembly: AssemblyDescription("MSBuild-based Visual Studio solution file generator")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SlnGen")]
 [assembly: AssemblyCopyright("Copyright ©  2017.  All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: InternalsVisibleTo("SlnGen.Build.Tasks.UnitTests")]
