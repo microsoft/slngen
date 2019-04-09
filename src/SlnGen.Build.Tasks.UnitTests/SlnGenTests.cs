ebd6abd8040df3537adbfa79095a7ff56ea17e98
Jeff Kluge <jeffkl@microsoft.com>
2019-04-09 16:31:29 -0700

    Fix support for cross targeting projects
    SDK-style projects dispatch inner builds to resolve project references.  This change updates the SlnGen target when running in cross targeting mode to dispatch the inner builds to get the project references.
    
    Fixes #50

--- src/SlnGen.Build.Tasks.UnitTests/SlnGenTests.cs
+++ src/SlnGen.Build.Tasks.UnitTests/SlnGenTests.cs
@@ -2,7 +2,10 @@
 //
 // Licensed under the MIT license.
 
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
 using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities.ProjectCreation;
 using Shouldly;
 using System;
 using System.Collections.Generic;
@@ -62,6 +65,87 @@ namespace SlnGen.Build.Tasks.UnitTests
             };
 
             ValidateParseCustomProjectTypeGuids(customProjectTypeGuids, ".foo", expectedProjectTypeGuid);
+        }
+
+        [Fact]
+        public void ProjectReferencesDeterminedInCrossTargetingBuild()
+        {
+            Dictionary<string, string> globlProperties = new Dictionary<string, string>
+            {
+                ["SlnGenLaunchVisualStudio"] = "false"
+            };
+
+            ProjectCollection projectCollection = new ProjectCollection(globlProperties);
+
+            ProjectCreator
+                .Create(Path.Combine(TestRootPath, "Directory.Build.props"))
+                .Save();
+            ProjectCreator
+                .Create(Path.Combine(TestRootPath, "Directory.Build.targets"))
+                .Import(Path.Combine(Environment.CurrentDirectory, "build", "SlnGen.targets"), condition: "'$(IsCrossTargetingBuild)' != 'true'")
+                .Import(Path.Combine(Environment.CurrentDirectory, "buildCrossTargeting", "SlnGen.targets"), condition: "'$(IsCrossTargetingBuild)' == 'true'")
+                .Save();
+
+            ProjectCreator projectA = ProjectCreator.Templates
+                .SdkCsproj(
+                    Path.Combine(TestRootPath, "ProjectA", "ProjectA.csproj"),
+                    targetFramework: "netcoreapp2.0")
+                .Save();
+
+            ProjectCreator projectB = ProjectCreator.Templates
+                .SdkCsproj(
+                    Path.Combine(TestRootPath, "ProjectB", "ProjectB.csproj"),
+                    targetFramework: "net46")
+                .Save();
+
+            ProjectCreator projectC = ProjectCreator.Templates
+                .SdkCsproj(
+                    Path.Combine(TestRootPath, "ProjectC", "ProjectC.csproj"),
+                    targetFramework: "net46")
+                .ItemProjectReference(projectA)
+                .Save();
+
+            ProjectCreator projectD = ProjectCreator.Templates
+                .SdkCsproj(
+                    Path.Combine(TestRootPath, "ProjectD", "ProjectD.csproj"),
+                    targetFramework: "net46")
+                .ItemProjectReference(projectB)
+                .ItemProjectReference(projectC)
+                .Save();
+
+            ProjectCreator
+                .Create(
+                    Path.Combine(TestRootPath, "ProjectC", "ProjectC.csproj"),
+                    sdk: "Microsoft.NET.Sdk",
+                    projectCollection: projectCollection,
+                    projectFileOptions: NewProjectFileOptions.None)
+                .PropertyGroup()
+                .Property("TargetFrameworks", "net46;netcoreapp2.0")
+                .Property("SlnGenAssemblyFile", Path.Combine(Environment.CurrentDirectory, "SlnGen.Build.Tasks.dll"))
+                .ItemProjectReference(projectA, condition: "'$(TargetFramework)' == 'netcoreapp2.0'")
+                .ItemProjectReference(projectB, condition: "'$(TargetFramework)' == 'net46'")
+                .ItemProjectReference(projectC, condition: "'$(TargetFramework)' == 'net46'")
+                .ItemProjectReference(projectD, condition: "'$(TargetFramework)' == 'net46'")
+                .Save()
+                .TryBuild("SlnGen", out bool result, out BuildOutput buildOutput, out IDictionary<string, TargetResult> targetOutputs);
+
+            result.ShouldBeTrue(buildOutput.GetConsoleLog());
+
+            KeyValuePair<string, TargetResult> targetOutput = targetOutputs.ShouldHaveSingleItem();
+
+            targetOutput.Key.ShouldBe("SlnGen");
+
+            targetOutput.Value.Items
+                .Select(i => i.GetMetadata("OriginalItemSpec"))
+                .ShouldBe(
+                    new[]
+                    {
+                        projectA.FullPath,
+                        projectB.FullPath,
+                        projectC.FullPath,
+                        projectD.FullPath
+                    },
+                    ignoreOrder: true);
         }
 
         [Fact]
