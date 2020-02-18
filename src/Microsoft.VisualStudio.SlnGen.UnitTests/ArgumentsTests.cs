b49156cdd8231f66aa23665a2bf4a80e67772187
Jeff Kluge <jeffkl@microsoft.com>
2020-02-18 08:51:39 -0800

    Support for /? and --nologo (#93)
    Also added `TestConsole` and some unit tests for the new arguments

---
+++ src/Microsoft.VisualStudio.SlnGen.UnitTests/ArgumentsTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation.
+//
+// Licensed under the MIT license.
+
+using Shouldly;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.SlnGen.UnitTests
+{
+    public class ArgumentsTests
+    {
+        [Fact]
+        public void ForwardSlashQuestionMarkDisplaysUsage()
+        {
+            TestConsole console = new TestConsole();
+
+            Program.Console = console;
+
+            int exitCode = Program.Main(new[] { "/?" });
+
+            exitCode.ShouldBe(0, console.Output);
+
+            console.Output.ShouldContain("Usage: slngen", console.Output);
+        }
+
+        [Theory]
+        [InlineData("--nologo")]
+        [InlineData("/nologo")]
+        public void NoLogoHidesLogo(string argument)
+        {
+            TestConsole console = new TestConsole();
+
+            Program.Console = console;
+
+            int exitCode = Program.Main(new[] { argument, "--help" });
+
+            exitCode.ShouldBe(0, console.Output);
+
+            console.OutputLines.First().ShouldStartWith("Usage: slngen", console.Output);
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.SlnGen.UnitTests/TestConsole.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) Microsoft Corporation.
+//
+// Licensed under the MIT license.
+
+using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Microsoft.VisualStudio.SlnGen.UnitTests
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IConsole" /> for unit tests to capture console output.
+    /// </summary>
+    public class TestConsole : IConsole
+    {
+        private readonly StringBuilder _error = new StringBuilder();
+        private readonly List<string> _errorLines = new List<string>();
+        private readonly StringBuilder _output = new StringBuilder();
+
+        private readonly List<string> _outputLines = new List<string>();
+
+        public TestConsole()
+        {
+            Error = new StringBuilderTextWriter(_error, _errorLines);
+
+            Out = new StringBuilderTextWriter(_output, _outputLines);
+        }
+
+        public event ConsoleCancelEventHandler CancelKeyPress
+        {
+            add { }
+            remove { }
+        }
+
+        public ConsoleColor BackgroundColor { get; set; }
+
+        public TextWriter Error { get; set; }
+
+        public IReadOnlyCollection<string> ErrorLines => _errorLines;
+
+        public string ErrorOutput => _error.ToString();
+
+        public ConsoleColor ForegroundColor { get; set; }
+
+        public TextReader In => throw new NotImplementedException();
+
+        public bool IsErrorRedirected => true;
+
+        public bool IsInputRedirected => throw new NotImplementedException();
+
+        public bool IsOutputRedirected => true;
+
+        public TextWriter Out { get; set; }
+
+        public string Output => _output.ToString();
+
+        public IReadOnlyCollection<string> OutputLines => _outputLines;
+
+        public void ResetColor()
+        {
+        }
+
+        private class StringBuilderTextWriter : TextWriter
+        {
+            private readonly List<string> _lines;
+            private readonly StringBuilder _lineStringBuilder = new StringBuilder();
+            private readonly StringBuilder _stringBuilder;
+
+            public StringBuilderTextWriter(StringBuilder stringBuilder, List<string> lines)
+            {
+                _stringBuilder = stringBuilder;
+                _lines = lines;
+            }
+
+            public override Encoding Encoding => Encoding.Unicode;
+
+            public override void Write(char value)
+            {
+                _lineStringBuilder.Append(value);
+
+                _stringBuilder.Append(value);
+
+                if (value == '\n')
+                {
+                    _lines.Add(_lineStringBuilder.ToString());
+
+                    _lineStringBuilder.Clear();
+                }
+            }
+        }
+    }
+}
